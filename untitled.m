91fe7afc42023671cf63883a8410f2f917c0776f
vinay-jaju <vinaykjaju@gmail.com>
2017-03-30 19:46:55 +0530

    accuracy 99.93%

--- untitled.m
+++ untitled.m
@@ -189,9 +189,9 @@ load('Theta2final.mat')
 srcFiles = dir('D:\captcha\captcha final\lol\*.png');
 valueSet =   {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
 keySet = {1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62};
-mapObj = containers.Map(keySet,valueSet)
+mapObj = containers.Map(keySet,valueSet);
 mapObj.Count
-ans = '';
+ans1 = '';
 %cd 'E:\BEProject\BEProjectNEW\lol\';
 %delete E:\BEProject\BEProjectNEW\lol\*.png;
 %cd 'E:\BEProject\BEProjectNEW\';
@@ -206,9 +206,9 @@ for i = 1 : length(srcFiles)
     a = a';
     x = predict(Theta1,Theta2,a)
     rain = mapObj(x)
-    ans = strcat(ans,rain)
+    ans1 = strcat(ans1,rain)
 end
-set(handles.text6, 'String', ans);
+set(handles.text6, 'String', ans1);
 
 
 % --- Executes on button press in pushbutton1.
